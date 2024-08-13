cf428889d26a4aa6915c5faa37c9ae2a98c8c652
Victor Ajibade <victor.ajibade@budpay.com>
2024-08-13 13:21:40 +0100

    started collections library

---
+++ src/BudPay.Net.SDK/Infrastructure/Integrations/HiBudPayClientIntegration.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using BudPay.Net.SDK.Interfaces.IIntegrations;
+using Newtonsoft.Json;
+
+namespace BudPay.Net.SDK.Infrastructure.Integrations;
+
+public class HiBudPayClientIntegration  : IHiBudPayClientIntegration
+{
+         private readonly HttpClient _client;
+        public HiBudPayClientIntegration(HttpClient httpClient)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            _client = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _client.DefaultRequestHeaders.Add("User-Agent", "Custom User Agent");
+            _client.Timeout = TimeSpan.FromMinutes(30);
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+        }
+
+
+            public async Task<T> GetAsync<T>(string relativePath, Dictionary<string, string>? content = null)
+        {
+            Uri requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture, relativePath));
+            var request = new HttpRequestMessage() { RequestUri = requestUrl, Method = HttpMethod.Get };
+
+            // request.Headers.Add("Bearer", StaticData.HiBudPayApiKey);
+
+            if (content != null) request.Content = new FormUrlEncodedContent(content);
+
+            var response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+            var data = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        private Uri CreateRequestUri(string relativePath, string queryString = "")
+        {
+            var endpoint = new Uri(relativePath);
+            var uriBuilder = new UriBuilder(endpoint);
+            if (!string.IsNullOrEmpty(queryString))
+            {
+                uriBuilder.Query = queryString;
+            }
+            return uriBuilder.Uri;
+        }
+
+        /// <summary>
+        /// Common method for making POST calls
+        /// </summary>
+        public async Task<T> PostAsync<T>(string relativePath, object content, string? token = null)
+        {
+            Uri requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture, relativePath));
+            var request = new HttpRequestMessage() { RequestUri = requestUrl, Method = HttpMethod.Post, Content = CreateHttpContent(content) };
+
+            if (token is not null) request.Headers.Add("Bearer", token);
+
+            var response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+            var data = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+
+        private HttpContent CreateHttpContent(object content)
+        {
+            var json = JsonConvert.SerializeObject(content, MicrosoftDateFormatSettings);
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+
+        private static JsonSerializerSettings MicrosoftDateFormatSettings
+        {
+            get
+            {
+                return new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+                };
+            }
+        }
+
+}
---
+++ src/BudPay.Net.SDK/Interfaces/IIntegrations/IHiBudPayClientIntegration.cs
@@ -0,0 +1,7 @@
+﻿namespace BudPay.Net.SDK.Interfaces.IIntegrations;
+
+public interface IHiBudPayClientIntegration
+{
+    Task<T> GetAsync<T>(string relativePath, Dictionary<string, string>? content = null);
+    Task<T> PostAsync<T>(string relativePath, object content, string? token = null);
+}
