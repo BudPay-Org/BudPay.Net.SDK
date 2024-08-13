48ab235c415f6575f1f888e84030a5f017d5b5a6
Victor Ajibade <victor.ajibade@budpay.com>
2024-08-13 14:42:45 +0100

    still builiding

---
+++ src/BudPay.Net.SDK/BaseConstant.cs
@@ -0,0 +1,6 @@
+﻿namespace BudPay.Net.SDK;
+
+public class BaseConstant
+{
+    public const string BudpayBaseUrl  =  "https://api.budpay.com/api/v2/";
+}
--- src/BudPay.Net.SDK/Interfaces/IIntegrations/IHiBudPayClientIntegration.cs
+++ src/BudPay.Net.SDK/Interfaces/IIntegrations/IHiBudPayClientIntegration.cs
@@ -2,6 +2,5 @@
 
 public interface IHiBudPayClientIntegration
 {
-    Task<T> GetAsync<T>(string relativePath, Dictionary<string, string>? content = null);
-    Task<T> PostAsync<T>(string relativePath, object content, string? token = null);
+    
 }
